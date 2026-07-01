725168e9fc6b9005ec282334c32e78ccb271cb76
Suraj Chouhan <Suraj@nwaytech.local>
2026-07-01 15:27:33 +0530

    message one

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL:
-    process.env.REACT_APP_API_URL
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
+
+console.log("API URL:", process.env.REACT_APP_API_URL);
+
+export default api;
